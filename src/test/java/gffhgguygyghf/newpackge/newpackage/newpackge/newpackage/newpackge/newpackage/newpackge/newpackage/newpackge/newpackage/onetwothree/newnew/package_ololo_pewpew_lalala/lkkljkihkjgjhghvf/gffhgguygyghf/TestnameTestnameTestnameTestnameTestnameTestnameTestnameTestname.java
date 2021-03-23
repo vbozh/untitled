7e8b170dd3cd98f4ea962cc7984ebcdc409d3cf0
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-23 16:19:16 +0300

    test

---
+++ src/test/java/gffhgguygyghf/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/onetwothree/newnew/package_ololo_pewpew_lalala/lkkljkihkjgjhghvf/gffhgguygyghf/TestnameTestnameTestnameTestnameTestnameTestnameTestnameTestname.java
@@ -0,0 +1,15 @@
+package gffhgguygyghf.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.onetwothree.newnew.package_ololo_pewpew_lalala.lkkljkihkjgjhghvf.gffhgguygyghf;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestnameTestnameTestnameTestnameTestnameTestnameTestnameTestname {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
