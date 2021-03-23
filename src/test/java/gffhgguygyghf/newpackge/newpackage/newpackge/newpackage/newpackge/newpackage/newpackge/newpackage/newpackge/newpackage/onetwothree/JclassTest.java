f355c1854f2823aa6487eecec1e96db55143ef31
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-23 17:12:39 +0300

    Commit message headline that needs to be one hundred twenty symbols and 1 symbol more to test headline in commit message. Commit message body that needs to be one hundred twenty symbols and 1 symbol more to test body length in commit messages.

---
+++ src/test/java/gffhgguygyghf/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/onetwothree/JclassTest.java
@@ -0,0 +1,15 @@
+package gffhgguygyghf.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.onetwothree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JclassTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
